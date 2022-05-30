cd04887fa415d8a1e031e32c403a4ccf65e14482
Helena Johansson <helena@klarr.se>
2022-05-30 10:30:28 +0200

    resources you need for the ratings ui app

---
+++ script.js
@@ -0,0 +1,4 @@
+//"server": "json-server --watch db.json --port 6001",
+//"dev": "concurrently \"npm run server\" \"npm start\""
+
+// copy the scripts and add to your package.json when you installed JSON server and concurrently
